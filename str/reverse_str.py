a9ce4ac036fe8b0a451cb551e1129ef64858ceaf
aryanmotghare77 <89638995+aryanmotghare77@users.noreply.github.com>
2022-04-14 09:09:18 +0530

    Promblems of Stringi in Python

---
+++ str/reverse_str.py
@@ -0,0 +1,4 @@
+def rev(str):
+    return str[::-1]
+
+print(rev("Geek For Geeks"))
---
+++ str/substr_present_in_str.py
@@ -0,0 +1,5 @@
+a = "Aryan Motgahre is a Content Creator and Learning Coding Skills"
+if "aryan" in a.lower():
+    print("Yes,  It is Present")
+else:
+    print("No, It is not Present")
